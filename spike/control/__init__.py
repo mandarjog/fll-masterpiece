e5991a15474ae4568a2343ab42b634151f7d52ce
Sheetal Jog <sheetalsahas@sheetals-mbp.lan>
2023-10-21 23:36:58 -0700

    Add API stubs

---
+++ spike/control/__init__.py
@@ -0,0 +1,33 @@
+
+class Timer:
+    def __init__(self):
+        """
+        To use the Timer, you must first initialiZe it.
+        """
+        pass
+    def reset(self):
+        """
+        Sets the Timer to "0."
+        """
+        pass
+    def now(self) -> int:   
+        """
+        The current time, specified in seconds.
+        """
+        pass
+        
+
+def wait_until(get_value_function, operator_function, target_value=True):
+    """
+    def red_or_position():
+        return color_sensor.get_color() == 'red' or motor.get_position() > 90
+
+    wait_until(red_or_position)
+    """
+    pass
+
+def wait_for_seconds(seconds: int):
+    """
+    Waits for a specified number of seconds before continuing the program.
+    """
+    pass
