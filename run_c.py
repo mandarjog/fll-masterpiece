402f453b6d16b8eadb12751f94501f5cc0608df7
Amish Shah <ashahengineer@gmail.com>
2023-11-11 14:11:42 -0800

    more recent changes for mission 5

--- run_c.py
+++ run_c.py
@@ -1,6 +1,4 @@
-# LEGO type:standard slot:2 autostart
-
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
@@ -9,8 +7,7 @@ from math import *
 hub = PrimeHub()
 front_motor = Motor('D')
 back_motor = Motor('C')
-left_color_sensor = ColorSensor('B')
-right_color_sensor = ColorSensor('F')
+
 motors = MotorPair('A', 'E')
 left_motor = Motor('A')
 right_motor = Motor('E')
@@ -19,19 +16,19 @@ right_motor = Motor('E')
 
 ## common code
 def gyro_straight(motion_sensor: MotionSensor, motor_pair: MotorPair,
-                  dist: float, single_motor:Motor, speed: int):
+                dist: float, single_motor:Motor, speed: int):
     motor_pair.move_tank(amount=dist, unit='cm', left_speed=speed, right_speed=speed)
 
 def gyro_turn2(motion_sensor: MotionSensor, motor_pair: MotorPair,
-               degrees: int, turn_speed=25, left_turn=False):
+            degrees: int, turn_speed=25, left_turn=False):
     if turn_speed < 20:
         raise ValueError("turn speed < 20")
-    
+
     if degrees < 0:
         raise ValueError("degrees < 0. Use left_turn=True")
 
     half_power = int(turn_speed/2)
-    if left_turn:  # turning left
+    if left_turn:# turning left
         half_power = -1 * half_power
 
     motor_pair.start_tank(half_power, -1*half_power)
@@ -83,22 +80,24 @@ def mission_2_3_5():
     print ("Total distance travelled", distance)
     gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=2, single_motor=left_motor, speed=30)
     #turn away from mission 2(towards mission 3)
+    angle_turn(motor_pair=motors, steer=100, speed=30, angle=45, stop=True, reset_angle=False)
+    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=-6, single_motor=left_motor, speed=40)
     angle_turn(motor_pair=motors, steer=100, speed=30, angle=90, stop=True, reset_angle=False)
     #go to mission 3
-    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=-30, single_motor=left_motor, speed=40)
+    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=-26, single_motor=left_motor, speed=40)
     #turn into mission 3
     angle_turn(motor_pair=motors, steer=-100, speed=20, angle=-90, stop=True)
     #ram into missioon 3 to align
     motors.move(0.5, 'seconds', 0, -50)
     #go backwards to prepare to do mission
-    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=4, single_motor=left_motor, speed=40)
+    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=5, single_motor=left_motor, speed=40)
     #do mission
     back_motor.run_for_degrees(-720, 100)
     wait_for_seconds(1)
-    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=2, single_motor=left_motor, speed=40)
+    gyro_straight(motion_sensor=hub.motion_sensor, motor_pair=motors, dist=1, single_motor=left_motor, speed=40)
     back_motor.run_for_degrees(400, 70)
     #turn away from mission 3
-    gyro_turn2(hub.motion_sensor, motors, degrees=90, turn_speed=25, left_turn=True)
+    angle_turn(motor_pair=motors, steer=100, speed=20, angle=90, stop=True)
 
     _mission_5()
 
@@ -107,12 +106,21 @@ def _mission_5():
     @cora
     """
     # mission 5
-    hub.light_matrix.write('A')
-    gyro_straight(hub.motion_sensor, motors, 6, left_motor, 50)
-    hub.light_matrix.write('B')
-    gyro_turn2(hub.motion_sensor, motors, degrees=50, turn_speed=25, left_turn=True)
-    gyro_straight(hub.motion_sensor, motors, 14, left_motor, 50)
-    front_motor.run_for_degrees(-300, 50)
-    gyro_turn2(hub.motion_sensor, motors, degrees=50, turn_speed=100)
-
+    gyro_straight(hub.motion_sensor, motors, -14, left_motor, 50)
+    angle_turn(motor_pair=motors, steer=-100, speed=20, angle=-50, stop=True)
+    gyro_straight(hub.motion_sensor, motors, -7, left_motor, 50)
+    back_motor.run_for_degrees(-300, 50)
+    angle_turn(motor_pair=motors, steer=100, speed=20, angle=25, stop=True)
+    # hub.light_matrix.write('A')
+    # gyro_straight(hub.motion_sensor, motors, -6, left_motor, 50)
+    # hub.light_matrix.write('B')
+    # gyro_turn2(hub.motion_sensor, motors, degrees=50, turn_speed=25, left_turn=True)
+    # gyro_straight(hub.motion_sensor, motors, 14, left_motor, 50)
+    # back_motor.run_for_degrees(-300, 50)
+    # gyro_turn2(hub.motion_sensor, motors, degrees=50, turn_speed=100)
+#_mission_5()
+# left_motor.run_to_position(0)
+# right_motor.run_to_position(0)
+# wait_for_seconds(2)
+#_mission_5()
 mission_2_3_5()
