6c57690199abca4f7d08e743b608cf185e581689
Amish Shah <ashahengineer@gmail.com>
2023-11-13 20:19:44 -0800

    new connor updates

--- run_c.py
+++ run_c.py
@@ -11,6 +11,7 @@ back_motor = Motor('C')
 motor_pair = MotorPair('A', 'E')
 left_motor = Motor('A')
 right_motor = Motor('E')
+right_motor = Motor('E')
 
 ONE_ROTATION_DISTANCE = 27.018
 hub.light_matrix.show_image('HAPPY')
@@ -19,8 +20,13 @@ front_motor = Motor('D')
 motor_pair.move_tank(-23, 'cm', left_speed=-50, right_speed=-50)
 front_motor.run_to_position(0)
 front_motor.run_for_degrees(300)
-motor_pair.move_tank(-3, 'cm', left_speed=-5, right_speed=-5)
+motor_pair.move_tank(-2, 'cm', left_speed=-5, right_speed=-5)
 front_motor.run_for_degrees(40)
 motor_pair.move_tank(5, 'cm', left_speed=10, right_speed=-10) 
-motor_pair.move_tank(20, 'cm', left_speed=-20, right_speed=-20)
+motor_pair.move_tank(10, 'cm', left_speed=-20, right_speed=-20)
+#motor_pair.move_tank(5, 'cm', left_speed=10, right_speed=0)
+#right_motor.run_to_position(90)
+motor_pair.move_tank(5, 'cm', left_speed=-100, right_speed=100)
+motor_pair.move_tank(-25, 'cm', left_speed=100, right_speed=100)
+
 
